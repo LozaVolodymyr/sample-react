ab105bedb566856642c0c2a333e34bb72e5352b6
Loza <vloza@paypal.com>
2019-12-04 10:51:15 -0800

    Updates

---
+++ precache-manifest.88517d5fd855fd933800b9e060cb15db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba4dc0a0015ca17ed03353f79b9f7335",
+    "url": "/sample-react/index.html"
+  },
+  {
+    "revision": "775f4fff8cbcf84f5d5c",
+    "url": "/sample-react/static/css/2.665f3fd3.chunk.css"
+  },
+  {
+    "revision": "b53f29e5b455add22b65",
+    "url": "/sample-react/static/css/main.ddd0aaef.chunk.css"
+  },
+  {
+    "revision": "775f4fff8cbcf84f5d5c",
+    "url": "/sample-react/static/js/2.ec593131.chunk.js"
+  },
+  {
+    "revision": "b53f29e5b455add22b65",
+    "url": "/sample-react/static/js/main.bd90ef26.chunk.js"
+  },
+  {
+    "revision": "c1dd78ac14d17843da60",
+    "url": "/sample-react/static/js/runtime-main.97748d2a.js"
+  }
+]);
